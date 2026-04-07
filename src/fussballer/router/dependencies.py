ff90af1548d5126fcffd67d288479eab1a08d7be
Nick Magin <mani1040@h-ka.de>
2026-04-07 12:27:50 +0200

    FussballerWriteRouter (fertig) + __init__ in Service angepasst

--- src/fussballer/router/dependencies.py
+++ src/fussballer/router/dependencies.py
@@ -5,7 +5,11 @@ from typing import Annotated
 from fastapi import Depends
 
 from fussballer.repository.fussballer_repository import FussballerRepository
+from fussballer.security.dependencies import get_user_service
+from fussballer.security.user_service import UserService
 from fussballer.service.fussballer_service import FussballerService
+from fussballer.service.fussballer_write_service import FussballerWriteService
+
 
 
 def get_repository() -> FussballerRepository:
@@ -18,3 +22,11 @@ def get_service(
 ) -> FussballerService:
     """Factory-Funktion für FussballerService."""
     return FussballerService(repo=repo)
+
+
+def get_write_service(
+    repo: Annotated[FussballerRepository, Depends(get_repository)],
+    user_service: Annotated[UserService, Depends(get_user_service)],
+) -> FussballerWriteService:
+    """Factory-Funktion für FussballerWriteService."""
+    return FussballerWriteService(repo=repo, user_service=user_service)
--- src/fussballer/service/__init__.py
+++ src/fussballer/service/__init__.py
@@ -1,15 +1,26 @@
 """Modul für den Zugriff auf Service Methoden."""
 
 from fussballer.service.adresse_dto import AdresseDTO
-from fussballer.service.exceptions import ForbiddenError, NotFoundError
+from fussballer.service.exceptions import (
+    EmailExistsError,
+    ForbiddenError,
+    NotFoundError,
+    UsernameExistsError,
+    VersionOutdatedError,
+)
 from fussballer.service.fussballer_dto import FussballerDTO
 from fussballer.service.fussballer_service import FussballerService
+from fussballer.service.fussballer_write_service import FussballerWriteService
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
 __all__ = [
     "AdresseDTO",
+    "EmailExistsError",
     "ForbiddenError",
     "FussballerDTO",
     "FussballerService",
+    "FussballerWriteService",
     "NotFoundError",
+    "UsernameExistsError",
+    "VersionOutdatedError",
 ]
