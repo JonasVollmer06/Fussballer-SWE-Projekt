773cbfc951a5ce1dda8caffdca8d8d2e7fa48dd9
Nick Magin <mani1040@h-ka.de>
2026-04-10 20:34:45 +0200

    find_by_id_test version 2

--- tests/unit/find_by_id_test.py
+++ tests/unit/find_by_id_test.py
@@ -43,4 +43,34 @@ def test_find_by_id_success(fussballer_service: FussballerService, session_mock)
 
 @mark.unit
 @mark.unit_find_by_id
-def test_find_by_id_not_found(fussballer_service:
+def test_find_by_id_not_found(fussballer_service: FussballerService, session_mock) -> None:
+    """Szenario 2: Fussballer existiert nicht in der Datenbank."""
+    fussballer_id = 999
+    user_mock = User("mocktest", "mock@test.de", "Mock",
+                     vorname="", roles=[Role.ADMIN], password="p")
+
+    session_mock.scalar.return_value = None
+
+    with raises(NotFoundError) as err:
+        fussballer_service.find_by_id(fusballer_id=fussballer_id, user=user_mock)
+    assert err.value.fussballer_id == fussballer_id
+
+
+@mark.unit
+@mark.unit_find_by_id
+def test_find_by_id_forbidden(fussballer_service: FussballerService, session_mock) -> None:
+    """Szenario 3: Zugriff auf fremdes Profil wird verweigert."""
+    fussballer_id = 1
+
+    # User heißt "other", Fussballer gehört aber "mocktest"
+    user_mock = User(username="other", email="other@test.de", nachname="Other", vorname="", roles=[Role.FUSSBALLER], password="p")
+    fussballer_mock = Fussballer(
+        id=fussballer_id, name="Mocktest", nationalitaet="DE", position=Position.STUERMER,
+        username="mocktest", email="mock@test.de", adresse=None, auszeichnungen=[]
+    )
+
+    session_mock.scalar.return_value = fussballer_mock
+
+    with raises(ForbiddenError) as err:
+        fussballer_service.find_by_id(fussballer_id=fussballer_id, user=user_mock)
+    assert err.type == ForbiddenError
