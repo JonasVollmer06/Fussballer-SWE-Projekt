9a37cb023427713f71b79ed7f28bbc6b9e72714a
Nick Magin <mani1040@h-ka.de>
2026-04-13 23:37:17 +0200

    test_get_by_email hinzugefügt (get_test.py)

--- tests/integration/rest/get_test.py
+++ tests/integration/rest/get_test.py
@@ -6,3 +6,37 @@ from typing import Final
 from common_test import ctx, login, rest_url
 from httpx import get
 from pytest import mark
+
+
+@mark.rest
+@mark.get_request
+@mark.parametrize("email", ["spieler1@test.de", "mock@test.de"])
+def test_get_by_email(email: str) -> None:
+    # arrange
+    params = {"email": email}
+    token: Final = login()
+    assert token is not None
+    headers = {"Authorization": f"Bearer {token}"}
+
+    # act
+    response: Final = get(
+        rest_url,
+        params=params,
+        headers=headers,
+        verify=ctx
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.OK
+    response_body: Final = response.json()
+    content: Final = response_body["content"]
+
+    assert isinstance(content, list)
+    assert len(content) == 1
+
+    fussballer = content[0]
+    assert fussballer is not None
+    assert fussballer.get("email") == email
+    assert fussballer.get("id") is not None
+
+    
