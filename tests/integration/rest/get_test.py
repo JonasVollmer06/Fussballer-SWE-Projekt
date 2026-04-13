859bb48c622c35f7cebde9492575cc30464661f6
Nick Magin <mani1040@h-ka.de>
2026-04-13 23:58:27 +0200

    test_get_nachnamen hinzugefügt (get_test.py)

--- tests/integration/rest/get_test.py
+++ tests/integration/rest/get_test.py
@@ -104,3 +104,30 @@ def test_get_by_nachname_not_found(nachname: str) -> None:
 
     #assert
     assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+
+@mark.rest
+@mark.get_request
+@mark.parametrize("teil", ["a", "n"])
+def test_get_nachnamen(teil: str) -> None:
+    # arrange
+    token: Final = login()
+    assert token is not None
+    headers = {"Authorization": f"Bearer {token}"}
+
+    # act
+    response: Final = get(
+        f"{rest_url}/nachnamen/{teil}",
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.OK
+    nachnamen: Final = response.json()
+    assert isinstance(nachnamen, list)
+    assert len(nachnamen) > 0
+
+    for nachname in nachnamen:
+        assert teil in nachname.lower()
