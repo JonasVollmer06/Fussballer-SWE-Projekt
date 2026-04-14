95e143132484979c56f2fecffadb1665dd25b9f6
Nick Magin <mani1040@h-ka.de>
2026-04-14 17:31:24 +0200

    test_post hinzugefügt (post_test.py)

--- tests/integration/rest/post_test.py
+++ tests/integration/rest/post_test.py
@@ -9,3 +9,39 @@ from httpx import post
 from pytest import mark
 
 token: str | None
+
+
+@mark.rest
+@mark.post_request
+def test_post() -> None:
+    # arrange
+    neuer_fussballer: Final = {
+        "nachname": "NeuerRest",
+        "nationalitaet": "DE",
+        "position": "STUERMER",
+        "username": "neu_rest",
+        "adresse": {
+            "plz": "99999",
+            "ort": "Restort",
+            "bundesland": "Restland"
+            },
+        "auszeichnungen": []
+    }
+    headers = {"Content_Type": "application/json"}
+
+    # act
+    response: Final = post(
+        rest_url,
+        json=neuer_fussballer,
+
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.CREATED
+    location: Final = response.headers.get("Location")
+    assert location is not None
+    int_pattern: Final = "[1-9][0-9]*$"
+    assert search(int_pattern, location) is not None
+    assert not response.text
