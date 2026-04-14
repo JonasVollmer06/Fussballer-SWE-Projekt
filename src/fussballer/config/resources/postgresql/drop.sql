ecd847b195ccdad5ba7a553df118c75eb6b6a746
Jonas Vollmer <jonas.vollmer15@gmail.com>
2026-04-14 19:50:04 +0200

    drop.sql fertig

--- src/fussballer/config/resources/postgresql/drop.sql
+++ src/fussballer/config/resources/postgresql/drop.sql
@@ -1,2 +1,12 @@
 DROP INDEX IF EXISTS
-    adresse_fussballer_id_idx
+    adresse_fussballer_id_idx,
+    auszeichnung_fussballer_id_idx,
+    fussballer_nachname_idx;
+
+DROP TABLE IF EXISTS
+    adresse,
+    auszeichnung,
+    fussballer;
+
+DROP TYPE IF EXISTS
+    position;
